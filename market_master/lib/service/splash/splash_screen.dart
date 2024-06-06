02703c8e4bbeaca63140b8b2017adb0297c9d562
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2024-06-07 02:47:52 +0530

    done demo splash screen

--- market_master/lib/service/splash/splash_screen.dart
+++ market_master/lib/service/splash/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:market_master/service/home/home_Screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,7 +12,92 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://w7.pngwing.com/pngs/686/527/png-transparent-fast-food-hamburger-sushi-pizza-fast-food-food-breakfast-fast-food-restaurant-thumbnail.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.black.withOpacity(0.9),
+                  const Color.fromARGB(255, 34, 189, 42).withOpacity(0.7),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          const Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 300.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.fastfood, size: 100),
+                      Padding(
+                        padding: EdgeInsets.only(right: 60, top: 20),
+                        child: Text(
+                          'Market ',
+                          style: TextStyle(fontSize: 30, color: Colors.white),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 60),
+                        child: Text(
+                          ' Master',
+                          style: TextStyle(fontSize: 30, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 20.0),
+                child: Column(
+                  children: [
+                    Divider(),
+                    SizedBox(height: 5),
+                    Text('© 2024 All rights reserved',
+                        style: TextStyle(fontSize: 14, color: Colors.white)),
+                    Text('Developed by Flutter Developer Com22',
+                        style: TextStyle(fontSize: 14, color: Colors.white)),
+                    SizedBox(height: 5),
+                    Text('Privacy Policy| Terms of Service| Contact Us',
+                        style: TextStyle(fontSize: 14, color: Colors.white)),
+                    Text('Follw us on Facebook Instergram Twitter',
+                        style: TextStyle(fontSize: 14, color: Colors.white)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
